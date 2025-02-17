df12d47cf217ae43419db255a74e34a80fb06d96
Mário Batista <mario.batista.oliveira3@gmail.com>
2025-02-17 16:26:14 -0300

    feat: updating the upload location for model images

--- feedback/models.py
+++ feedback/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Feedback(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     occupation = models.CharField(max_length=255, blank=True, null=True)
-    user_image = models.ImageField(upload_to='user_image/', blank=True, null=True)
+    user_image = models.ImageField(upload_to='images/userImage/', blank=True, null=True)
     opinion = models.TextField(blank=True, null=True)
 
     def __str__(self):
--- vehicles/models.py
+++ vehicles/models.py
@@ -6,7 +6,7 @@ class Vehicle(models.Model):
     car_type = models.CharField(max_length=255, blank=True, null=True)
     quantity_seats = models.IntegerField(blank=True, null=True)
     quantity_luggage = models.IntegerField(blank=True, null=True)
-    car_image = models.ImageField(upload_to='vehicles/', blank=True, null=True)
+    car_image = models.ImageField(upload_to='images/vehicles/', blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     car_overview = models.TextField(blank=True, null=True)
     car_amenities = models.TextField(blank=True, null=True)
