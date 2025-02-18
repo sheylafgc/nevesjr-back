eaeb05fd3fc8aafe8e5a43cf04285d7947cf5dbd
Mário Batista <mario.batista.oliveira3@gmail.com>
2025-02-18 11:41:48 -0300

    feat: add urls media root and static files

--- nevesjr_api/urls.py
+++ nevesjr_api/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import permissions
@@ -48,3 +50,7 @@ urlpatterns = [
     path('api/', include('feedback.urls', namespace="feedback")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
