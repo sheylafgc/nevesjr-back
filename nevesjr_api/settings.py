ce5c681cf1a04243054be09ebd09df2e1dabd931
Mário Batista <mario.batista.oliveira3@gmail.com>
2025-02-18 11:37:13 -0300

    feat: updating the upload location for model images

--- nevesjr_api/settings.py
+++ nevesjr_api/settings.py
@@ -156,6 +156,9 @@ AUTH_USER_MODEL = 'users.User'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
