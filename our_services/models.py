8ccc1518b6c3bfda2d0b845e75a774d5a14d338a
Mário Batista <mario.batista.oliveira3@gmail.com>
2025-02-18 11:37:13 -0300

    feat: updating the upload location for model images

--- our_services/models.py
+++ our_services/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class OurService(models.Model):
     title = models.TextField(blank=True, null=True)
     subtitle = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='images/ourServices/', blank=True, null=True)
+    image = models.ImageField(upload_to='ourServices/', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
