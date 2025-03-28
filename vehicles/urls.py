39d4ac9380245343aea18ae19c74629439c39b0f
Mário Batista <mario.batista.oliveira3@gmail.com>
2025-03-27 23:04:12 -0300

    feat: updating urls.py from vehicles

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -6,8 +6,8 @@ app_name = 'vehicle'
 
 
 urlpatterns = [
-    path('vehicle', VehicleListAPIView.as_view(), name='vehicle-list'),
-    path('vehicle/<int:pk>', VehicleDetailAPIView.as_view(), name='vehicle-detail'),
+    path('vehicle/', VehicleListAPIView.as_view(), name='vehicle-list'),
+    path('vehicle/<int:pk>/', VehicleDetailAPIView.as_view(), name='vehicle-detail'),
     path('vehicle/create', VehicleCreateAPIView.as_view(), name='vehicle-create'),
     path('vehicle/update/<int:pk>', VehicleUpdateAPIView.as_view(), name='vehicle-update'),
     path('vehicle/delete/<int:pk>', VehicleDeleteAPIView.as_view(), name='vehicle-delete'),
