8b7d8744a3d40dbcdef00b2cb66c5f8cacd4d453
Mário Batista <mario.batista.oliveira3@gmail.com>
2025-03-27 22:49:07 -0300

    feat: add forms.py from vehicles

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
 from .models import Vehicle
+from .forms import VehicleForm
 
 
-admin.site.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    form = VehicleForm
+    exclude = ['car_name', 'car_type', 'car_overview', 'car_amenities', 'car_best_for_services',]
+
+admin.site.register(Vehicle, VehicleAdmin)
