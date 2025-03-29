41e876e759182a295780ce14c01c97c2f581e02a
Mário Batista <mario.batista.oliveira3@gmail.com>
2025-03-29 20:18:34 -0300

    feat: update views.py

--- home_page/views.py
+++ home_page/views.py
@@ -6,6 +6,10 @@ from django.utils.translation import activate
 
 from .models import HomePage
 from .serializers import HomePageSerializer
+from feedback.serializers import FeedbackSerializer
+from frequently_questions.serializers import FrequentlyQuestionsSerializer
+from our_services.serializers import OurServiceSerializer
+from vehicles.serializers import VehicleSerializer
 
 from drf_yasg.utils import swagger_auto_schema
 
@@ -18,7 +22,7 @@ class HomePageAPIView(APIView):
         lang = request.GET.get('lang')
 
         if not lang:
-            return Response({'error': 'The ‘lang’ parameter is required.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'The “lang” parameter is required.'}, status=status.HTTP_400_BAD_REQUEST)
 
         activate(lang)
 
@@ -33,7 +37,7 @@ class HomePageAPIView(APIView):
             'section1_title': getattr(home_page, f'section1_title_{lang}', ''),
             'section2_title': getattr(home_page, f'section2_title_{lang}', ''),
             'section2_image': serializer.data['section2_image'],
-            'section2_services': serializer.data['section2_services'],
+            'section2_services': OurServiceSerializer(home_page.section2_services.all(), many=True).data,
             'section3_title': getattr(home_page, f'section3_title_{lang}', ''),
             'section3_image': serializer.data['section3_image'],
             'differentials': serializer.data['differentials'],
@@ -42,12 +46,12 @@ class HomePageAPIView(APIView):
             'section4_description': getattr(home_page, f'section4_description_{lang}', ''),
             'section5_title': serializer.data['section5_title'],
             'section5_subtitle': getattr(home_page, f'section5_subtitle_{lang}', ''),
-            'section5_feedbacks': serializer.data['section5_feedbacks'],
+            'section5_feedbacks': FeedbackSerializer(home_page.section5_feedbacks.all(), many=True).data,
             'section6_title': getattr(home_page, f'section6_title_{lang}', ''),
             'section6_subtitle': getattr(home_page, f'section6_subtitle_{lang}', ''),
-            'section6_vehicles': serializer.data['section6_vehicles'],
+            'section6_vehicles': VehicleSerializer(home_page.section6_vehicles.all(), many=True).data,
             'section7_title': getattr(home_page, f'section7_title_{lang}', ''),
-            'section7_frequently_questions': serializer.data['section7_frequently_questions'],
+            'section7_frequently_questions': FrequentlyQuestionsSerializer(home_page.section7_frequently_questions.all(), many=True).data,
             'section8_title': getattr(home_page, f'section8_title_{lang}', ''),
             'section8_banner': serializer.data['section8_banner'],
         }
