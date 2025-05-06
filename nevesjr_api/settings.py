9270be34993faf737914e2fb91422776251a554f
Mário Batista <mario.batista.oliveira3@gmail.com>
2025-05-06 12:37:34 -0300

    fix: correcao de bug

--- nevesjr_api/settings.py
+++ nevesjr_api/settings.py
@@ -145,6 +145,10 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET NAMES 'utf8mb4'",
+        },
     }
 }
 
