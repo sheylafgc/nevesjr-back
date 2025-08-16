23b32917edf7994cca0729dcd63ac86f15f010e3
Mário Batista <mario.batista.oliveira3@gmail.com>
2025-08-15 22:41:29 -0300

    feat: update settings

--- nevesjr_api/settings.py
+++ nevesjr_api/settings.py
@@ -25,8 +25,7 @@ pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR / ".env") 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -229,3 +228,17 @@ LANGUAGES = [
     ('pt', _('Portuguese')),
     ('es', _('Spanish')),
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
